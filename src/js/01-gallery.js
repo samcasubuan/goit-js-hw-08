6928b74cd08689a073decedc8402a6c95ab36c26
samcasubuan <s.mcasubuan23@yahoo.com>
2023-11-29 19:11:48 +0800

    end hw 8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,42 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+// import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+// Described in documentation;
+
+import SimpleLightbox from 'simplelightbox';
+// Additional styles import
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryList = document.querySelector('.gallery');
+
+const createGallery = el => {
+  return el
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+   </li>
+
+  `;
+    })
+    .join('');
+};
+
+const photoMarkup = createGallery(galleryItems);
+galleryList.insertAdjacentHTML('beforeend', photoMarkup);
+
+//---------------------------------------------------------
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+lightbox.on('show.simplelightbox');
